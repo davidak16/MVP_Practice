098b1ec8880e2b66cbd6d66fa18f905e2460ba82
David Ak <davidakerele16@gmail.com>
2026-03-16 16:38:48 -0400

    Problem 1

--- sketch_mar16a_ProblemSet1.ino
+++ sketch_mar16a_ProblemSet1.ino
@@ -3,7 +3,19 @@ void setup() {
   Serial.begin(115200);
   delay(1000);
 
-  
+  Serial.printf("%d", sizeof(char));
+  Serial.printf("%d \n", sizeof(int));
+  Serial.printf("%d \n", sizeof(uint8_t));
+  Serial.printf("%d \n", sizeof(uint16_t));
+  Serial.printf("%d \n", sizeof(uint32_t));
+  Serial.printf("%d \n", sizeof(int16_t));
+  Serial.printf("%d \n", sizeof(float));
+  Serial.printf("%d", sizeof(double));
+
+  uint8_t x = 255;
+  x = x + 1;
+  Serial.print("uint8_t overflow: ");
+  Serial.println(x);
 
 }
 
